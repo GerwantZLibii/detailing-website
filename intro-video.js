c3e90f829c7b836866f4b21316d2d8b536f1768e
GerwantZLibii <patrykprokopski@outlook.com>
2024-08-10 01:23:35 +0200

    skibidi

--- intro-video.js
+++ intro-video.js
@@ -1,6 +1,5 @@
 window.addEventListener('scroll', function() {
     var videoContainer = document.getElementById('header-video');
-    console.log(window.scrollY);
     if (window.scrollY > 250) {
         videoContainer.classList.add('shrink');
     } else {
